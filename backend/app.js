a8b976f950b27fa0f722f4dd6e03efbcf578ff1f
Denton Kev <dentonkev122@gmail.com>
2024-05-24 11:37:52 +1000

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,28 @@
 import express from 'express';
+import morgan from 'morgan';
 const app = express()
 const port = 3200
 
-// Import route modules
-// import timetableRoutes from './routes/timetable'
-// import userRoutes from './routes/user'
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Use route modules
-// app.use('/timetable', timetableRoutes)
-// app.use('/user', userRoutes)
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
+
+app.use((req, res) => {
+  const error = `
+    404 Not found - This could be because:
+      0. You have defined routes below (not above) this middleware in server.ts
+      1. You have not implemented the route ${req.method} ${req.path}
+      2. There is a typo in either your test or server, e.g. /posts/list in one
+         and, incorrectly, /post/list in the other
+      3. You are using ts-node (instead of ts-node-dev) to start your server and
+         have forgotten to manually restart to load the new changes
+      4. You've forgotten a leading slash (/), e.g. you have posts/list instead
+         of /posts/list in your server.ts or test file
+  `;
+});
