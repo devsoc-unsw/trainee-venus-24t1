0b2ad7d5ddcea9be6bde12e0d79f09002d90889f
Denton Kev <dentonkev122@gmail.com>
2024-05-24 11:32:03 +1000

    .

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,16 @@ const app = express()
 const port = 3200
 
 // Import route modules
-import timetableRoutes from './routes/timetable'
-import userRoutes from './routes/user'
+// import timetableRoutes from './routes/timetable'
+// import userRoutes from './routes/user'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 // Use route modules
-app.use('/timetable', timetableRoutes)
-app.use('/user', userRoutes)
+// app.use('/timetable', timetableRoutes)
+// app.use('/user', userRoutes)
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
